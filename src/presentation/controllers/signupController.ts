0591b3526f8e0f0d4e60a9bc3c5b405f09943964
Herik Santos <heriksantos04@gmail.com>
2024-06-20 12:23:57 -0300

    feat: ensure SignupController return 400 if does no exist passwordConfimation

--- src/presentation/controllers/signupController.ts
+++ src/presentation/controllers/signupController.ts
@@ -4,13 +4,16 @@ import { type IHttpRequest, type IHttpResponse } from "./protocols/http";
 
 class SignupController {
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const { name, email, password } = httpRequest.body;
+    const { name, email, password, passwordConfirmation } = httpRequest.body;
 
     if (!name) return badRequest(new MissingParamError("name"));
 
     if (!email) return badRequest(new MissingParamError("email"));
 
     if (!password) return badRequest(new MissingParamError("password"));
+
+    if (!passwordConfirmation)
+      return badRequest(new MissingParamError("passwordConfirmation"));
 
     return {
       statusCode: 200,
