0460effc104810793acb6189255da51c2b16cdc8
Herik Santos <heriksantos04@gmail.com>
2024-06-20 15:12:22 -0300

    feat: ensure signupController return 400 if email is not valid

--- src/presentation/controllers/signupController.ts
+++ src/presentation/controllers/signupController.ts
@@ -1,11 +1,29 @@
+import { InvalidParamError } from "../errors/invalid-params-error";
 import { MissingParamError } from "../errors/missing-params-error";
 import { badRequest } from "../helpers/ttp-helper";
 import { type IController } from "./protocols/controller";
+import { type IEmailValidator } from "./protocols/email-validator";
 import { type IHttpRequest, type IHttpResponse } from "./protocols/http";
 
 class SignupController implements IController {
+  private readonly emailValidator: IEmailValidator;
+
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const { name, email, password, passwordConfirmation } = httpRequest.body;
+    const {
+      name,
+      email,
+      password,
+      passwordConfirmation,
+    }: {
+      name: string;
+      email: string;
+      password: string;
+      passwordConfirmation: string;
+    } = httpRequest.body;
 
     if (!name) return badRequest(new MissingParamError("name"));
 
@@ -15,6 +33,9 @@ class SignupController implements IController {
 
     if (!passwordConfirmation)
       return badRequest(new MissingParamError("passwordConfirmation"));
+
+    if (!this.emailValidator.isValid(email))
+      return badRequest(new InvalidParamError("email"));
 
     return {
       statusCode: 200,
