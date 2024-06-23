5f33fedbc049c6583f2a9529f88410d25a614f5c
Herik Santos <heriksantos04@gmail.com>
2024-06-23 11:48:53 -0300

    refactor: creation makeSut

--- src/data/useCases/add-account/db-add-account.spec.ts
+++ src/data/useCases/add-account/db-add-account.spec.ts
@@ -1,21 +1,38 @@
+import { type IEncrypter } from "../../protocols/encrypter";
 import { DbAddAccountUseCase } from "./db-add-acount";
+
+interface ISutTypes {
+  encrypterStub: IEncrypter;
+  sut: DbAddAccountUseCase;
+}
+
+const makeSut = (): ISutTypes => {
+  class EncrypterStub {
+    async encrypt(password: string): Promise<string> {
+      return "hashed_password";
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+
+  const sut = new DbAddAccountUseCase(encrypterStub);
+
+  return {
+    encrypterStub,
+    sut,
+  };
+};
 
 describe("DbAccount UseCase", () => {
   it("Should call encrypter with correct password", async () => {
-    class EncrypterStub {
-      async encrypt(password: string): Promise<string> {
-        return "hashed_password";
-      }
-    }
-    const escrypterStub = new EncrypterStub();
-    const sut = new DbAddAccountUseCase(escrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const accountData = {
       name: "valid_name",
       email: "valid_email",
       password: "valid_password",
     };
 
-    const cryptSpy = jest.spyOn(escrypterStub, "encrypt");
+    const cryptSpy = jest.spyOn(encrypterStub, "encrypt");
 
     await sut.add(accountData);
 
