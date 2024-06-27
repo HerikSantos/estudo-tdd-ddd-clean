33f70a738ccad414d32547096c5ffdbc868cb92a
Herik Santos <heriksantos04@gmail.com>
2024-06-27 15:27:34 -0300

    refactor: ensure call bd before run test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,9 +1,29 @@
 // eslint-disable-next-line
 import request from "supertest";
 
+import { env } from "../../enviroment";
+import { mongoHelper } from "../../infra/db/mongodb/Account-repository/helpers/mong-helper";
 import { app } from "../config/app";
 
 describe("Signup Routes", () => {
+  beforeAll(async () => {
+    try {
+      await mongoHelper.connect(env.MONGO_URL);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = mongoHelper.getDbCollection("accounts");
+
+    await accountCollection?.deleteMany({});
+  });
+
   it("Should return an account on success ", async () => {
     await request(app)
       .post("/signup")
